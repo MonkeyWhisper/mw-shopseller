b4ea064153db235b863372fca657016032f2723d
MonkeyWhisper <82112471+MonkeyWhisper@users.noreply.github.com>
2022-01-18 23:18:16 -0500

    Config
    Updated for config to grab price from config

--- server/main.lua
+++ server/main.lua
@@ -1,29 +1,27 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-RegisterServerEvent("mw-shopseller:server:sellItems")
-AddEventHandler("mw-shopseller:server:sellItems", function()
+RegisterNetEvent('mw-shopseller:server:sellItems', function()
     local src = source
-	local Player = QBCore.Functions.GetPlayer(src)
-	local price = 0
-    if Player.PlayerData.items ~= nil and Player.PlayerData.job.name == "pawnshop" then 
-        for k, v in pairs(Player.PlayerData.items) do 
-            if Player.PlayerData.items[k] ~= nil then 
-                if Player.PlayerData.items[k].name == "rolex" then 
-                    price = price + (Config.Items["rolex"]["price"] * Player.PlayerData.items[k].amount)
-                    Player.Functions.RemoveItem("rolex", Player.PlayerData.items[k].amount, k)
-				elseif Player.PlayerData.items[k].name == "goldbar" then 
-					price = price + (Config.Items["goldbar"]["price"] * Player.PlayerData.items[k].amount)
-					Player.Functions.RemoveItem("goldbar", Player.PlayerData.items[k].amount, k)
-				end
+    local price = 0
+    local Player = QBCore.Functions.GetPlayer(src)
+    if Player.PlayerData.items ~= nil and next(Player.PlayerData.items) ~= nil then
+        for k, v in pairs(Player.PlayerData.items) do
+            if Player.PlayerData.items[k] ~= nil then
+                if Config.Items[Player.PlayerData.items[k].name] ~= nil then
+                    price = price + (Config.Items[Player.PlayerData.items[k].name].price * Player.PlayerData.items[k].amount)
+                    Player.Functions.RemoveItem(Player.PlayerData.items[k].name, Player.PlayerData.items[k].amount, k)
+                    TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items[Player.PlayerData.items[k].name], "remove")
+                end
             end
         end
         Player.Functions.AddMoney("cash", price)
         TriggerEvent("qb-bossmenu:server:addAccountMoney", 'pawnshop', price*0.30)
-		TriggerClientEvent('QBCore:Notify', src, "Sold, check your cash!")
-	else 
+        TriggerClientEvent('QBCore:Notify', src, "You have sold your items")
+    else 
 		TriggerClientEvent('QBCore:Notify', src, "You are not a pawnshop employee!")
 	end
 end)
+
 
 
 -- Uncomment Below if you do not want a job check
