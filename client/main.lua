99855ffeb6910ca102e3aa3806aab944a4fb10b2
MonkeyWhisper <82112471+MonkeyWhisper@users.noreply.github.com>
2022-01-23 14:00:53 -0500

    Optional Ped
    Optional spawn ped creator if you don't want to use ms-peds or qb-target ped spawning.

--- client/main.lua
+++ client/main.lua
@@ -28,3 +28,28 @@ exports['qb-target']:AddBoxZone("pawnshopseller", vector3(158.44, -1313.71, 29.3
 			 },
 		distance = 1.5
 })
+
+-- Optional spawn ped creator if you don't want to use ms-peds or qb-target ped spawning. 
+--[[ Citizen.CreateThread(function()
+	spawnPed1()
+end)
+
+local ped1 = nil
+local hasbeenrobbed = false
+
+-- Pawnshop Ped
+function spawnPed1()
+if ped1 == nil and not hasbeenrobbed then
+  RequestModel(GetHashKey('g_m_m_mexboss_01'))
+  while not HasModelLoaded(GetHashKey('g_m_m_mexboss_01')) do
+  Wait(1)
+  end
+  ped1 = CreatePed(2, GetHashKey('g_m_m_mexboss_01'), 106.40, -1281.03, 28.80, 9.94, false, false) -- change here the cords for the ped 
+  SetPedFleeAttributes(ped1, 0, 0)
+  SetPedDiesWhenInjured(ped1, false)
+  TaskStartScenarioInPlace(ped1, "WORLD_HUMAN_STAND_IMPATIENT", 0, true)
+  SetPedKeepTask(ped1, true)
+  SetBlockingOfNonTemporaryEvents(ped1, true)
+  SetEntityInvincible(ped1, true)
+  end
+end ]]
